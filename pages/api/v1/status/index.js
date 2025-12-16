ca823a78cedac2b1ad97726ef172fed1063d3872
Enzo Rincon <enzorincon2003@gmail.com>
2025-12-16 19:55:57 +0000

    finalizando o endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,27 @@
 import database from "/infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
-  res
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são pessoas acima da média!" });
+  const updateAt = new Date().toISOString();
+  const versionPostgres = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const usesConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  res.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version_db: versionPostgres.rows[0].server_version,
+        max_connection_db: parseInt(maxConnections.rows[0].max_connections),
+        open_connections_db: usesConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
