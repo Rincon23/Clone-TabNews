f6ea52b65e12881f1b718c7b8d993783509b2d53
Enzo Rincon <enzorincon2003@gmail.com>
2025-12-29 14:37:38 +0000

    adds `/api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,29 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(res1.status).toBe(201);
+
+  const res1ponseBody = await res1.json();
+
+  expect(Array.isArray(res1ponseBody)).toBe(true);
+  expect(res1ponseBody.length).toBeGreaterThan(0);
+
+  const res2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(res2.status).toBe(200);
+
+  const res2ponseBody = await res2.json();
+
+  expect(Array.isArray(res2ponseBody)).toBe(true);
+  expect(res2ponseBody.length).toBe(0);
+});
