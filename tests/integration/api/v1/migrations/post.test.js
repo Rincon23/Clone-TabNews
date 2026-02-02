7fd303816c4c0da66db40c8a0ba13a623dca21ad
Enzo Rincon <enzorincon2003@gmail.com>
2026-02-02 16:09:21 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,24 +6,31 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pendig migrations", () => {
+      test("For the first time", async () => {
+        const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+        expect(res1.status).toBe(201);
+
+        const res1ponseBody = await res1.json();
+
+        expect(Array.isArray(res1ponseBody)).toBe(true);
+        expect(res1ponseBody.length).toBeGreaterThan(0);
+      });
+      test("For the second time", async () => {
+        const res2 = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+        expect(res2.status).toBe(200);
+
+        const res2ponseBody = await res2.json();
+
+        expect(Array.isArray(res2ponseBody)).toBe(true);
+        expect(res2ponseBody.length).toBe(0);
+      });
+    });
   });
-  expect(res1.status).toBe(201);
-
-  const res1ponseBody = await res1.json();
-
-  expect(Array.isArray(res1ponseBody)).toBe(true);
-  expect(res1ponseBody.length).toBeGreaterThan(0);
-
-  const res2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(res2.status).toBe(200);
-
-  const res2ponseBody = await res2.json();
-
-  expect(Array.isArray(res2ponseBody)).toBe(true);
-  expect(res2ponseBody.length).toBe(0);
 });
